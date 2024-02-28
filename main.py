64b5e606eb39e19d8ce9292f234106e3228da168
KovTon <ikovtonogov@gmail.com>
2024-02-28 22:12:52 +0400

    1.Add option 4.Выйти. It gives opportunity to provide as many operations as u want befor shutdown.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def add_user(name, email, age):
 def delet_user(queri):
     connection = sqlite3.connect('pet_base.db')
     cursor = connection.cursor()
-    cursor.execute(f"DELETE FROM Users WHERE username = '{queri}' OR email = '{queri}' OR age = {queri} ")
+    cursor.execute(f"DELETE FROM Users WHERE username LIKE'%{queri}%' OR email LIKE '%{queri}%' OR age LIKE '{queri}'")
     connection.commit()
     connection.close()
 
@@ -20,14 +20,14 @@ def delet_user(queri):
 def get_user(queri):
     connection = sqlite3.connect('pet_base.db')
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM Users WHERE username = '{queri}' OR email = '{queri}' OR age = {queri} ")
+    cursor.execute(f"SELECT * FROM Users WHERE username LIKE'%{queri}%' OR email LIKE '%{queri}%' OR age LIKE '{queri}' ")
     search_results = cursor.fetchall()
 
     # Выводим результаты
     for _ in search_results:
         print(_)
 
-    connection.commit() 
+    connection.commit()
     connection.close()
 
 
@@ -35,23 +35,27 @@ functions = '''
 1. Добавить человека.
 2. Найти человека.
 3. Удалить человека.
+4. Выйти
 '''
+choice = int()
 
-print(functions)
-choice = int(input())
+while choice != 4:
+    print(functions)
+    print('Введите цифру')
+    choice = int(input())
 
-if choice == 1:
-    name = input('Введите имя: ')
-    email = input('Введите email: ')
-    age = int(input('Введите возраст: '))
-    add_user(name, email, age)
-    print('Данные добавлены')
+    if choice == 1:
+        name = input('Введите имя: ')
+        email = input('Введите email: ')
+        age = int(input('Введите возраст: '))
+        add_user(name, email, age)
+        print('Данные добавлены')
 
-elif choice == 2:
-    queri = input('Введите точные данные для поиска: ')
-    get_user(queri)
+    elif choice == 2:
+        queri = input('Введите имя, email или возраст для поиска: ')
+        get_user(queri)
 
-elif choice == 3:
-    queri = input('Введите параметр точные данные для удаления: ')
-    print(delet_user(queri))
-    print(f"Строка с полем {queri} удалена.")
+    elif choice == 3:
+        queri = input('Введите параметр точные данные для удаления: ')
+        print(delet_user(queri))
+        print(f"Строка с полем {queri} удалена.")
